70a2e3ef0f6cdaeb6464db50faecf299e1828a55
PraveenMadiwal <praveenpavi633@gmail.com>
2026-04-28 13:47:50 +0530

    add footer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,42 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Built by <strong>Praveen Kumar M S</strong> |
+        <a
+          href="https://github.com/PraveenMadiwal/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        {" | "}
+        <a
+          href="https://praveenwebpage.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Portfolio
+        </a>
+      </p>
+    </footer>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      <div className="app-wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:name" element={<Detail />} />
+        </Routes>
 
-        {/* Home Page */}
-        <Route path="/" element={<Home />} />
-
-        {/* Detail Page */}
-        <Route path="/detail/:name" element={<Detail />} />
-
-      </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
